78c68a7ea5588f990f77a7d21981abd3ef32a611
nguyenvankhai1 <59248489+nguyenvankhai1@users.noreply.github.com>
2021-01-12 14:42:25 +0700

    commit

--- web_view/pages/detai/index.vue
+++ web_view/pages/detai/index.vue
@@ -27,6 +27,60 @@
             </v-slide-group>
           </v-sheet>
         </v-col>
+        <v-col cols="7">
+          <div class="headline">
+            Áo thun nam tay dài mặc thu đông, giữ nhiệt, chất cotton co giãn, áo
+            dài tay dáng ôm cao cấp
+          </div>
+          <div class="d-flex pt-3 color-text">
+            <ins class="">4.8</ins>
+            <v-rating
+              :value="5"
+              class="ratinga pr-3"
+              background-color="orange lighten-3"
+              style="color:#EE4D2D; margin-top: -3px"
+              small
+            ></v-rating>
+            <div class="evaluate pr-3">
+              <ins class="black--text pl-3">24</ins>
+              Đánh giá
+            </div>
+            <div class="evaluate d-flex">
+              <div class="black--text pl-3 pr-1 subtitle-2">112</div>
+              <div>Đã bán</div>
+            </div>
+          </div>
+          <div class="display-1 py-6 color-text d-flex">
+            <div class="title">₫</div>
+            99.000
+          </div>
+          <v-row>
+            <v-col cols="2">Vẫn Chuyển</v-col>
+            <v-col cols="10"></v-col>
+            <v-col cols="2">Size</v-col>
+            <v-col cols="10" class="pt-3 pl-0">
+              <v-btn outlined small color="#767676">
+                <div>XL (45-57kg)</div>
+              </v-btn></v-col
+            >
+            <v-col cols="2" class="pb-12">Số lượng</v-col>
+            <v-col cols="10"></v-col>
+          </v-row>
+          <div>
+            <v-btn large style="border: 1px solid;" depressed color="#FFE6DE">
+              Thêm Vào Giỏ Hàng
+            </v-btn>
+            <v-btn large class="white--text" depressed color="#F05D40">
+              Mua Ngày
+            </v-btn>
+          </div>
+          <v-divider class="mt-4 mr-3"></v-divider>
+          <v-row>
+            <v-col cols="4">7 ngày miễn phí trả hàng</v-col>
+            <v-col cols="4">Hàng chính hãng 100%</v-col>
+            <v-col cols="4">Miễn phí vận chuyển</v-col>
+          </v-row>
+        </v-col>
       </v-row>
     </v-card>
   </div>
@@ -41,7 +95,6 @@ export default {
       model: null,
       imgs: [
         'https://thehekhoinghiep.com/wp-content/uploads/2018/07/Cach-chup-anh-quan-ao-ban-hang-online-dep-12.jpg',
-        'https://lh3.googleusercontent.com/proxy/iLfveHFg9lcUKIN-ZCrU4NJJdZpy0jEbITKblLOxQicKTqt4gNNIs6F7nva11XMLjIm80pQDFbushTmPhE7105koq9eNYKR9QpW4lixp3G1ZFjA99Q3If433U8OsY-in15_IgvoiKQ',
         'https://cf.shopee.vn/file/6090ee3c0ac0f296530478f8277e9685',
         'https://chupanh.vn/wp-content/uploads/2018/01/chup-anh-quan-ao-3-1.jpg',
         'https://vn-test-11.slatic.net/p/c50313205bc62651242efe79f2095224.jpg'
@@ -60,6 +113,17 @@ export default {
 
 <style lang="scss">
 .detaiProduct {
+  .evaluate {
+    color: #767676;
+    border-left: 1px solid #767676;
+    margin-top: -2px;
+    font-size: 14px;
+  }
+  .ratinga {
+    button {
+      padding: 1px !important;
+    }
+  }
   .v-slide-group__prev.v-slide-group__prev--disabled {
     position: absolute;
     z-index: 1;
@@ -73,6 +137,9 @@ export default {
   }
   .cardProduct {
     border: 1px solid white !important;
+  }
+  .color-text {
+    color: #ee4d2d;
   }
   .cardProduct:hover {
     border: 1px solid #ee4d2d !important;
